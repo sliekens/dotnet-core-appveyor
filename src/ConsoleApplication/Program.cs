a7b4e1d655a9c0e73e8a3421e808fe133cb834d0
Steven Liekens <steven.liekens@gmail.com>
2016-06-30 17:55:05 +0200

    Add netstandard1.6 sample

--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication
             Console.WriteLine(LibNETStandard13.Class1.Message);
             Console.WriteLine(LibNETStandard14.Class1.Message);
             Console.WriteLine(LibNETStandard15.Class1.Message);
+            Console.WriteLine(LibNETStandard16.Class1.Message);
         }
     }
 }
